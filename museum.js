5ec9b7b385b1d9015e4bda75ef91d42582ad4f9c
tedbeast <tedbalashov@gmail.com>
2022-09-08 12:17:57 -0400

    made a basic painting site

--- museum.js
+++ museum.js
@@ -4,8 +4,27 @@ console.log(document);
 
 let button = document.getElementById('loadButton');
 let content = document.getElementById('container');
-button.onclick = loadSpan;
+button.onclick = loadAllPaintings;
 
-function loadSpan(){
-    content.innerText = "button clicked placeholder";
+function loadSpan(response){
+    for(let i = 0; i < response.length; i++){
+        let paintingTitle = document.createElement("p");
+        paintingTitle.innerText = response[i].title;
+        content.appendChild(paintingTitle);
+    }
+}
+function loadAllPaintings(){
+    let request = new XMLHttpRequest();
+    request.open("GET", "http://localhost:9000/paintings");
+    request.send();
+
+    request.onreadystatechange = load;
+
+    function load(){
+        if(request.readyState == 4 && request.status == 200){
+            let responsejson = JSON.parse(request.response);
+            console.log(responsejson);
+            loadSpan(responsejson);
+        }
+    }
 }
